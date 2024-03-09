b9c73df72c3b888c01b04fc5d55ffaf9f9e65ded
yassirelkhaili <elkhailiyassir@gmail.com>
2024-03-09 10:38:49 +0100

    saving progress

--- index.php
+++ index.php
@@ -3,25 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./normalize.css">
     <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./normalize.css">
     <script src="./sandbox.js"></script>
     <title>Super awesome calendar</title>
 </head>
 <body>
-    <!-- calendar toggle button -->
-    <button class="toggle-calendar">Toggle calendar</button>
+   <section class="first-section">
+     <!-- calendar toggle button -->
+     <button class="toggle-calendar">Toggle Calendar</button>
     <!-- calendar -->
-    <div id="calendar" class="hidden calendar">
-        <h2 class="calendar__title">September 2022</h2>
+    <div id="calendar" class="calendar">
+       <div class="calendar__nav__container">
         <div class="calendar__nav">
             <button type="button" class="calendar__nav__button">
-                <img src="./assets/left-arrow.svg" alt="left-calendar-arrow">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6"></path></svg>
             </button>
+            <h2 class="calendar__title">September 2022</h2>
             <button type="button" class="calendar__nav__button">
-                <img src="./assets/right-arrow.svg" alt="right-calendar-arrow">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"></path></svg>
             </button>
+       </div>
         </div>
+        <div class="calendar__body">
+        <div class="calendar__body__days">
+            <span class="calendar__body__dayoftheweek">Su</span><span class="calendar__body__dayoftheweek">Mo</span><span class="calendar__body__dayoftheweek">Tu</span><span class="calendar__body__dayoftheweek">We</span><span class="calendar__body__dayoftheweek">Th</span><span class="calendar__body__dayoftheweek">Fr</span><span class="calendar__body__dayoftheweek">Sa</span>
+        </div>
+        <div class="calendar__body__cells">
+        <span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span>
+        </div>
+       </div>
     </div>
+   </section>
 </body>
 </html>
