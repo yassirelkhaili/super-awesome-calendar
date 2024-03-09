cbf929ae2be9f104b49210d67cf52d6d50c5f31b
yassirelkhaili <elkhailiyassir@gmail.com>
2024-03-09 15:44:40 +0100

    saving progress

--- index.php
+++ index.php
@@ -16,11 +16,11 @@
     <div id="calendar" class="calendar">
        <div class="calendar__nav__container">
         <div class="calendar__nav">
-            <button type="button" class="calendar__nav__button" style="padding-right: 0px;">
+            <button type="button" class="calendar__nav__button" id="prevButton" style="padding-right: 0px;">
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6"></path></svg>
             </button>
             <h2 class="calendar__title">September 2022</h2>
-            <button type="button" class="calendar__nav__button" style="padding-left: 0px;">
+            <button type="button" class="calendar__nav__button" id="nextButton" style="padding-left: 0px;">
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"></path></svg>
             </button>
        </div>
--- sandbox.js
+++ sandbox.js
@@ -3,26 +3,62 @@
  * @license MIT
 */
 
+const currentYear = new Date().getMonth(); //get current month (from date object)
+const currentMonth = new Date().getFullYear(); //get current year (from date object)
+
 document.addEventListener("DOMContentLoaded", () => {
     // toggle calendar onclick
     const calendar = document.getElementById("calendar");
     const toggleButton = document.querySelector(".toggle-calendar");
     toggleButton && toggleButton.addEventListener("click", () => calendar && calendar.classList.toggle("hidden"));
-    
-    //render calendar
+
+    //clear calendar cells
     const calendarCellContainer = document.querySelector('.calendar__body__cells');
 
+    const clearCalendarCells = () => {
+        while (calendarCellContainer.firstChild) calendarCellContainer.removeChild(calendarCellContainer.firstChild);
+    }
 
     //generate calendar cell
-    const generateCalendarCell = (content) => {
+    const generateCalendarCell = (content, currentMonth) => {
         const cell = document.createElement("span");
         cell.classList.add('calendar__body__cell', 'calendar--hover');
+        currentMonth ? cell.classList.add('calendar__body__cell--white') : cell.classList.add('calendar__body__cell--grey');
         cell.textContent = content;
         return cell;
     }
-
-    //clear calendar cells
-    const clearCalendarCells = () => {
-        while (calendarCellContainer.children) calendarCellContainer.removeChild(calendarCellContainer.firstChild);
+    
+    //render calendar
+    const renderCalendarCells = (currentYear, currentMonth) => {
+        clearCalendarCells(); //reset calendar content
+        
     }
+
+    renderCalendarCells(currentYear, currentMonth); //render initial calendar values
+
+    //add event listeners
+    const nextButton = document.getElementById('nextButton');
+    const prevButton = document.getElementById('prevButton');
+
+    nextButton && nextButton.addEventListener('click', () => { //handle next button click
+        //add one month
+        if (currentMonth === 11) {
+            currentMonth = 0;
+            currentYear += 1;
+        } else {
+            currentMonth += 1;
+        }
+        renderCalendarCells(currentYear, currentMonth);
+    })
+
+    prevButton && prevButton.addEventListener('click', () => {  //handle prev button click
+        //substract one month
+        if (currentMonth === 0) {
+            currentMonth = 11;
+            currentYear -= 1;
+        } else {
+            currentMonth -= 1;
+        }
+        renderCalendarCells(currentYear, currentMonth);
+    })
 })
