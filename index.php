fe56636d3063f06916b13ca314511d4fbb6d0f37
yassirelkhaili <elkhailiyassir@gmail.com>
2024-03-09 18:24:55 +0100

    saving prgress

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
                     <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                 </svg>
             </div>
-            <input datepicker type="text" class="datepicker__input" placeholder="Select date">
+            <input datepicker type="text" class="datepicker__input" placeholder="Select date" id="datepicker__input">
         </div>
         <!-- calendar -->
         <div id="calendar" class="calendar">
--- sandbox.js
+++ sandbox.js
@@ -9,14 +9,15 @@ let currentYear = new Date().getFullYear(); //get current year (from date object
 document.addEventListener("DOMContentLoaded", () => {
     // toggle calendar onclick
     const calendar = document.getElementById("calendar");
-    const toggleButton = document.querySelector(".toggle-calendar");
+    const calendarInput = document.getElementById("datepicker__input");
+    const calenderInputContainer = document.querySelector(".toggle-calendar");
 
-    toggleButton && toggleButton.addEventListener("click", () => calendar && calendar.classList.remove("hidden"));
+    calenderInputContainer && calenderInputContainer.addEventListener("click", () => calendar && calendar.classList.remove("hidden"));
 
     //close calendar on outsideclick
     const handleCalendarOutsideClick = (event) => {
         const target = event.target;
-        if (!calendar.contains(target) && !toggleButton.contains(target)) calendar && calendar.classList.add("hidden");
+        if (!calendar.contains(target) && !calenderInputContainer.contains(target)) calendar && calendar.classList.add("hidden");
     }
 
     document.addEventListener("click", handleCalendarOutsideClick);
@@ -27,12 +28,34 @@ document.addEventListener("DOMContentLoaded", () => {
         while (calendarCellContainer.firstChild) calendarCellContainer.removeChild(calendarCellContainer.firstChild);
     }
 
+    //handle cell click
+    const handleCellClick = (format, daySelected) => {
+        let fullDate = '';
+        const month = (currentMonth + 1).toString().padStart(2, '0');
+        const paddedDay = daySelected.toString().padStart(2, '0');
+        switch(format) {
+            case 'ISO':
+                console.log("accessed")
+                fullDate = `${currentYear}-${month}-${paddedDay}`
+            break;
+            case 'US':
+                fullDate = `${currentYear}/${month}/${paddedDay}`
+            break;
+            default:
+            const monthName = new Date(currentYear, currentMonth).toLocaleString('default', { month: 'long' });
+            fullDate = `${paddedDay}-${monthName}-${currentYear}`;
+            break;
+        }
+        calendarInput.value = fullDate;
+    }
+
     //generate calendar cell
     const generateCalendarCell = (content, currentMonth = true) => {
         const cell = document.createElement("span");
         cell.classList.add('calendar__body__cell', 'calendar--hover');
         currentMonth ? cell.classList.add('calendar__body__cell--white') : cell.classList.add('calendar__body__cell--grey');
         cell.textContent = content;
+        cell.addEventListener("click", () => handleCellClick('ISO', content));
         calendarCellContainer.appendChild(cell);
     }
 
