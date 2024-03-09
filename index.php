342499ffe6c2de97651fb84d1b492e86e4e4ab9a
yassirelkhaili <elkhailiyassir@gmail.com>
2024-03-09 17:00:39 +0100

    save progress

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             <button type="button" class="calendar__nav__button" id="prevButton" style="padding-right: 0px;">
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6"></path></svg>
             </button>
-            <h2 class="calendar__title" id="calendarDisplay">September 2022</h2>
+            <h2 class="calendar__title" id="calendarDisplay"></h2>
             <button type="button" class="calendar__nav__button" id="nextButton" style="padding-left: 0px;">
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"></path></svg>
             </button>
@@ -29,9 +29,7 @@
         <div class="calendar__body__days">
             <span class="calendar__body__dayoftheweek">Su</span><span class="calendar__body__dayoftheweek">Mo</span><span class="calendar__body__dayoftheweek">Tu</span><span class="calendar__body__dayoftheweek">We</span><span class="calendar__body__dayoftheweek">Th</span><span class="calendar__body__dayoftheweek">Fr</span><span class="calendar__body__dayoftheweek">Sa</span>
         </div>
-        <div class="calendar__body__cells">
-        <span class="calendar__body__cell calendar--hover">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span><span class="calendar__body__cell">12</span>
-        </div>
+        <div class="calendar__body__cells"></div>
        </div>
     </div>
    </section>
--- sandbox.js
+++ sandbox.js
@@ -3,8 +3,8 @@
  * @license MIT
 */
 
-const currentMonth = new Date().getMonth(); //get current month (from date object) (indexed from 0)
-const currentYear = new Date().getFullYear(); //get current year (from date object)
+let currentMonth = new Date().getMonth(); //get current month (from date object) (indexed from 0)
+let currentYear = new Date().getFullYear(); //get current year (from date object)
 
 document.addEventListener("DOMContentLoaded", () => {
     // toggle calendar onclick
@@ -20,12 +20,12 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     //generate calendar cell
-    const generateCalendarCell = (content, currentMonth) => {
+    const generateCalendarCell = (content, currentMonth = true) => {
         const cell = document.createElement("span");
         cell.classList.add('calendar__body__cell', 'calendar--hover');
         currentMonth ? cell.classList.add('calendar__body__cell--white') : cell.classList.add('calendar__body__cell--grey');
         cell.textContent = content;
-        return cell;
+        calendarCellContainer.appendChild(cell);
     }
 
     //update date display
@@ -42,8 +42,16 @@ document.addEventListener("DOMContentLoaded", () => {
         const firstDayOfMonth = new Date(selectedYear, selectedMonth, 1).getDay(); //0 for sunday (╯°□°）╯︵ ┻━┻ 
         const numOfDaysInMonth = new Date(selectedYear, selectedMonth + 1, 0).getDate();
         const numOfDaysInPrevMonth = new Date(selectedYear, selectedMonth, 0).getDate();
-        // update display
+        //update display
         updateDateDisplay(selectedYear, selectedMonth);
+        //generate cells of previous month days
+        for (let i = firstDayOfMonth - 1; i >= 0; i--) generateCalendarCell(numOfDaysInPrevMonth - i, false);
+        //generate cells of current month days
+        for (let i = 1; i <= numOfDaysInMonth; i++) generateCalendarCell(i);
+        const totalDaysShown = firstDayOfMonth + numOfDaysInMonth;
+        let nextMonthDays = 42 - totalDaysShown; //42 being the total cell number in the calendar
+        //generate the remaining cells (next month)
+        for (let i = 1; i <= nextMonthDays; i++) generateCalendarCell(i, false);
     }
 
     renderCalendarCells(currentYear, currentMonth); //render initial calendar values
