724a10d89106126d9b4eab1406213ac4d93b9185
yassirelkhaili <elkhailiyassir@gmail.com>
2024-03-09 11:21:36 +0100

    saving progress

--- sandbox.js
+++ sandbox.js
@@ -9,4 +9,20 @@ document.addEventListener("DOMContentLoaded", () => {
     const toggleButton = document.querySelector(".toggle-calendar");
     toggleButton && toggleButton.addEventListener("click", () => calendar && calendar.classList.toggle("hidden"));
     
+    //render calendar
+    const calendarCellContainer = document.querySelector('.calendar__body__cells');
+
+
+    //generate calendar cell
+    const generateCalendarCell = (content) => {
+        const cell = document.createElement("span");
+        cell.classList.add('calendar__body__cell', 'calendar--hover');
+        cell.textContent = content;
+        return cell;
+    }
+
+    //clear calendar cells
+    const clearCalendarCells = () => {
+        while (calendarCellContainer.children) calendarCellContainer.removeChild(calendarCellContainer.firstChild);
+    }
 })
