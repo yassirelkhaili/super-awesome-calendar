61399d878b8c07119a2a62ae949e1490177cc9fd
yassirelkhaili <elkhailiyassir@gmail.com>
2024-03-11 22:27:35 +0000

    better file structure

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
-    <link rel="stylesheet" href="./normalize.css">
-    <script type="module" src="./sandbox.js"></script>
+    <link rel="stylesheet" href="./styles/style.css">
+    <link rel="stylesheet" href="./styles/normalize.css">
+    <script type="module" src="./js/sandbox.js"></script>
     <title>Super awesome calendar</title>
 </head>
 
